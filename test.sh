b98818592602c1496955f72992cf489fd4c08e14
RahulRj09 <rahul18@navgurukul.org>
2019-05-13 12:55:30 +0530

    implementation test cases

--- test.sh
+++ test.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+javac LinearSearch.java
+java LinearSearch 34 > actual
+diff actual expectedOutput
+java LinearSearch 100 > actual
+diff actual expectedOutput1
+java LinearSearch 98 > actual
+diff actual expectedOutput2
