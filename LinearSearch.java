ab3d951db69d6a81a3be8888d7a417b1daa53998
RahulRj09 <rahul18@navgurukul.org>
2019-05-13 12:47:26 +0530

    refactor loop use fori instead of forin

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,13 +1,13 @@
 public class LinearSearch {
     private static void numberChecker(int userInput) {
         int[] listOfNumbers = {12, 34, 45, 98, 23};
-        for (int number : listOfNumbers){
-            if (number == userInput){
-                System.out.println(number);
+        for (int i = 0; i < listOfNumbers.length; i++) {
+            if (listOfNumbers[i] == userInput){
+                System.out.println("Number in list and index of number "+i);
                 return;
             }
         }
-        System.out.println("invalid input");
+        System.out.println("Number not in list");
     }
 
     public static void main(String[] args){
